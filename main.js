46b821ae7f40522fdfbef308135c0dc22a861302
GHOSTTPROG <96992358+GHOSTTPROG@users.noreply.github.com>
2023-01-26 23:21:52 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 const yargs = require("yargs");
 const chalk = require("chalk");
+const showBanner = require('node-banner');
+const figlet = require('figlet');
+const CLI = require('clui'),
+    Spinner = CLI.Spinner;
+ 
+
+    var countdown = new Spinner('The program will run in 3 seconds ... ', ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']);
+ 
+countdown.start();
+ 
+var number = 3;
+setInterval(function () {
+  number--;
+  countdown.message('The program will run in ' + number + ' seconds... ');
+  if (number === 0) {
+    process.stdout.write('\n');
+    process.exit(0);
+  }
+}, 1000);
+
+figlet('GHOSTEPROG', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+    console.log("   Please Type ( node main.js --help )")
+});
 
 const { addContact, listContacts, removeContact } = require("./contacts");
+
 
 yargs.scriptName(`${chalk.yellow("Contact Manager")}`);
 yargs.usage(`$0 ${chalk.red("<Command>")} ${chalk.green("[Args]")}`);
